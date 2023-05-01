fad4cfecd5f6536619cb3ca7a7a1e132d086c89d
Debanjan Banerjee <banerjee.deba@gmail.com>
2023-05-01 10:58:27 -0400

    updated title

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lunch Box',
+      title: 'nom nom - Lunch Box',
       theme: ThemeData(
         // This is the theme of your application.
         //
